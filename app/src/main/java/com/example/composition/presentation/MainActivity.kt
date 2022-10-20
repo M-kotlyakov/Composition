a5213bff016ab0c346f072f4002aa8d5d093e327
Max <m.kotlyakov.02@mail.ru>
2022-10-20 22:14:47 +0300

    initial commit

--- app/src/main/java/com/example/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composition/presentation/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.composition.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.composition.R
+import com.example.composition.databinding.ActivityMainBinding
+import com.example.composition.databinding.FragmentGameBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() =  _binding ?: throw RuntimeException("ActivityMainBinding == null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate( layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
