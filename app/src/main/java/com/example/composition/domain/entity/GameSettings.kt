840825fb95f17c0f0e5fe7e401ad179678f5d836
Max <m.kotlyakov.02@mail.ru>
2022-10-27 18:28:32 +0300

    added dataBinding

--- app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
@@ -2,7 +2,6 @@ package com.example.composition.domain.entity
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-import java.io.Serializable
 
 @Parcelize
 data class GameSettings(
